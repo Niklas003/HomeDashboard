efd82c3f77bc23aa5ae31b1cbf193b47f4ffba2f
Niklas <nlabitzke77@gmail.com>
2023-06-30 19:29:04 +0200

    feat: added icons

--- src/components/SidebarNavigation.vue
+++ src/components/SidebarNavigation.vue
@@ -3,16 +3,20 @@
     <div>
       <router-link to="/" class="nav-box-active">
         <div class="nav-outer-box-active">
-          <span class="material-symbols-rounded"> home </span>
+          <span class="material-symbols-rounded">home</span>
         </div>
         <b>Home</b>
       </router-link>
       <router-link to="" class="nav-box-inactive">
-        <div class="nav-outer-box-active"></div>
+        <div class="nav-outer-box-inactive">
+          <span class="material-symbols-rounded">settings</span>
+        </div>
         Einstellungen
       </router-link>
       <router-link to="" class="nav-box-inactive">
-        <div class="nav-outer-box-active"></div>
+        <div class="nav-outer-box-inactive">
+          <span class="material-symbols-rounded">help</span>
+        </div>
         Statusübersicht
       </router-link>
     </div>
@@ -20,15 +24,27 @@
 </template>
 <script setup lang="ts"></script>
 
-<style>
+<style lang="scss">
 .nav-outer-box-active {
   border-radius: 0.5rem;
   background: linear-gradient(228deg, #1daf36 0%, #95eb2c 100%);
   box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.1);
-  min-width: 2.25rem;
-  min-height: 2.25rem;
+  padding: 0.5rem;
   flex-shrink: 0;
-  margin-right: 0.25rem;
+  margin-right: 1rem;
+
+  span {
+    color: white;
+  }
+}
+
+.nav-outer-box-inactive {
+  border-radius: 0.5rem;
+  background: #fff;
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.1);
+  padding: 0.5rem;
+  flex-shrink: 0;
+  margin-right: 1rem;
 }
 
 .nav-box-inactive {
